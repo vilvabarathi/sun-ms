10df6d24e76a84af82e434d05246265f4b2ddba9
unknown <vilvabharathi@gmail.com>
2024-07-30 15:31:33 +0530

    pushcode

---
+++ src/main/java/com/example/sun_ms/SunMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sun_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunMsApplication.class, args);
+	}
+
+}
